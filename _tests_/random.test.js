3b452a75565a567d8e078656c1aa4695d524cab5
UCDT <adrian.j.ulibarri@gmail.com>
2021-05-23 13:40:42 -0600

    relocating to starbucks for coffee and chill

---
+++ _tests_/random.test.js
@@ -0,0 +1,6 @@
+const { TestScheduler } = require('@jest/core');
+const checkIfEqual = require('../lib/random.js');
+
+test('checks if 10 is equal to 10', () => {
+    expect(checkIfEqual(10, 10)).toBe(true)
+});
