ff765b0000661e84914d47aac6fc2fd1b8bcde43
UCDT <adrian.j.ulibarri@gmail.com>
2021-05-23 18:37:06 -0600

    finished 10.1.8

--- _tests_/random.test.js
+++ _tests_/random.test.js
@@ -1,6 +1,6 @@
-const { TestScheduler } = require('@jest/core');
-const checkIfEqual = require('../lib/random.js');
+const randomNumber = require('../lib/random');
 
-test('checks if 10 is equal to 10', () => {
-    expect(checkIfEqual(10, 10)).toBe(true)
+test('generates a random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
 });
