0e6aa1ead855c9c1d25738958055f2765d564d3a
UCDT <adrian.j.ulibarri@gmail.com>
2021-05-24 16:02:09 -0600

    added isAlive, getHealth and reduceHealth prototypes and tests

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
@@ -34,3 +35,32 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+test("getplayer's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('check if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+})
